35373d1d4fa4a026c8065805a1122ac7b4c6b232
Alexander Meinhardt Scheurer <ams@42patents.com>
2018-10-11 18:58:29 +0200

    Update install script to include indent feature

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ mkdir -p ~/.vim/ftdetect/
 cp ./ftdetect/fut.vim ~/.vim/ftdetect/
 mkdir -p ~/.vim/syntax/
 cp ./syntax/fut.vim ~/.vim/syntax/
+mkdir -p ~/.vim/indent/
+cp ./indent/fut.vim ~/.vim/indent/
